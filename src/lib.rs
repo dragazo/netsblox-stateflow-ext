5746a0d9f3f6d087ab74dbf013a5f99193e0f938
Devin Jean <emailcruzjean@yahoo.com>
2024-07-16 15:42:00 -0500

    dep refactor

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,11 @@ use netsblox_stateflow::*;
 use wasm_bindgen::prelude::*;
 use netsblox_extension_macro::*;
 use netsblox_extension_util::*;
-use web_sys::js_sys::*;
-use web_sys::*;
+use js_helpers::*;
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(msg: &str);
-}
-
-macro_rules! js {
-    ($root:ident) => { $root.clone() };
-    ($root:ident . $f:ident ( $($args:expr),*$(,)? )) => { Reflect::apply(&js!($root.$f).dyn_ref().unwrap(), &$root, &(vec![$($args),*] as Vec<JsValue>).into_iter().collect()) };
-    ($root:ident . $path:ident $($rest:tt)*) => {{
-        let zzz = Reflect::get(&$root, &stringify!($path).into()).unwrap_or_else(|_| JsValue::undefined());
-        js!(zzz $($rest)*)
-    }};
-    ($root:ident [ $idx:expr ] $($rest:tt)*) => {{
-        let zzz = $root.dyn_ref::<Array>().map(|arr| arr.get($idx)).unwrap_or_else(|| JsValue::undefined());
-        js!(zzz $($rest)*)
-    }};
 }
 
 #[netsblox_extension_info]
@@ -31,14 +17,13 @@ pub const INFO: ExtensionInfo = ExtensionInfo {
 #[wasm_bindgen]
 #[netsblox_extension_menu_item("Visualize")]
 pub fn visualize() {
-    let window = window().unwrap().dyn_into::<JsValue>().unwrap();
     let xml = js!(window.world.children[0].getSerializedRole()).unwrap().as_string().unwrap();
     match Project::compile(&xml, None) {
         Ok(proj) => {
             let graphviz_code = graphviz::print(proj.to_graphviz(), &mut Default::default());
-            let encoded = js!(window.encodeURIComponent(graphviz_code.into())).unwrap().as_string().unwrap();
+            let encoded = js_sys::encode_uri_component(&graphviz_code).as_string().unwrap();
             let url = format!("https://dreampuf.github.io/GraphvizOnline/#{encoded}");
-            js!(window.open(url.into(), "_blank".into())).unwrap();
+            js!(window.open(url, "_blank")).unwrap();
         }
         Err(e) => alert(&format!("visualize error: {e:?}")),
     }
